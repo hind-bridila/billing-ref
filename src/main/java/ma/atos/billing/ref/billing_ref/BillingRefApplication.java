dcda75e35dca831532f183cb54660ab70545775b
hind-bridila <hbridila.hind@gmail.com>
2026-05-15 15:50:34 +0200

    First commit

---
+++ src/main/java/ma/atos/billing/ref/billing_ref/BillingRefApplication.java
@@ -0,0 +1,13 @@
+package ma.atos.billing.ref.billing_ref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingRefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingRefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/atos/billing/ref/billing_ref/BillingRefApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.atos.billing.ref.billing_ref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingRefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
